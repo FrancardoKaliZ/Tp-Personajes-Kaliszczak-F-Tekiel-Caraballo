c2c19fe2c1bc2a398f290ba7eddc16c7af319424
JULIAN CARABALLO <46603069@ort>
2023-05-12 12:11:30 -0300

    Falta la parte de Visual Studio

---
+++ index.js
@@ -0,0 +1,5 @@
+import personajeServices from './services/personaje-services.js';
+import express from 'express';
+import sql from 'mssql'
+const app = express();
+const port = 3000;
---
+++ models/Peliculas.js
@@ -0,0 +1,8 @@
+class Pelicula {
+    Id;
+    Imagen;
+    Titulo;
+    FechaDeCreacion;
+    Calificaion; 
+}
+export default Pelicula;
